5ec1043752f151489b008cdcca42aa49e91e5d04
igorunderplayer <igorschug6@gmail.com>
2024-02-17 01:00:29 -0300

    remove outlines

--- web/src/lib/components/NoteTextArea/index.tsx
+++ web/src/lib/components/NoteTextArea/index.tsx
@@ -1,6 +1,6 @@
 const NoteTextArea: React.FC = () => {
   return (
-    <textarea className="w-full h-full bg-transparent text-zinc-100 resize-none border-none outline-violet-400 outline-1 p-2"></textarea>
+    <textarea className="w-full h-full bg-transparent text-zinc-100 resize-none border-none outline-none outline-1 p-2"></textarea>
   );
 };
 
--- web/src/lib/components/NoteTittle/index.tsx
+++ web/src/lib/components/NoteTittle/index.tsx
@@ -2,7 +2,7 @@ const NoteTitle: React.FC = () => {
   return (
     <div className="border-b-2 border-white text-4xl text-zinc-100 font-light pt-4 ">
       <input
-        className="bg-transparent w-full h-full p-4 outline-violet-400"
+        className="bg-transparent w-full h-full p-4 outline-none"
         type="text"
         placeholder="Título da sua nota..."
       />

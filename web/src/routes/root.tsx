a6c580a9aba91da3537a1099038cd847eeaef2f6
igorunderplayer <igorschug6@gmail.com>
2024-02-18 01:33:40 -0300

    create placeholder root page

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -1,26 +1,22 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { auth } from "../lib/firebase";
-import { useAuth } from "../lib/hooks/useAuth";
-
 import { Link } from "react-router-dom";
 
 const RootPage: React.FC = () => {
-  const { user } = useAuth(auth);
   return (
-    <main className="flex w-screen h-screen bg-zinc-900">
-      <h1>This is the root route!! 😀</h1>
-
-      {!user ? (
-        <button onClick={() => signInWithPopup(auth, new GoogleAuthProvider())}>
-          Logar com google
-        </button>
-      ) : (
-        <div>
-          <p>Olá {user.displayName}</p>
-
-          <Link to="/notes">O</Link>
-        </div>
-      )}
+    <main className="flex flex-col items-center justify-center w-screen h-screen bg-zinc-900 ">
+      <h1 className="text-zinc-100 font-light text-2xl p-4">
+        Welcome, this is not the final root page
+      </h1>
+      <nav className="bg-zinc-800 rounded-lg flex flex-col gap-2 text-zinc-950 p-4">
+        <Link className="bg-cyan-200 p-2 rounded-md" to="/notes">
+          Go to notes &gt;
+        </Link>
+        <Link className="bg-emerald-200 p-2 rounded-md" to="/login">
+          Go to login &gt;
+        </Link>
+        <Link className="bg-purple-200 p-2 rounded-md" to="/register">
+          Go to register &gt;
+        </Link>
+      </nav>
     </main>
   );
 };

a3123d70549aad34c22b60db6a9402a133318554
igorunderplayer <igorschug6@gmail.com>
2024-02-18 00:56:38 -0300

    remove unused save button

--- web/src/lib/components/NoteTextArea/index.tsx
+++ web/src/lib/components/NoteTextArea/index.tsx
@@ -1,7 +1,4 @@
-import { doc, updateDoc } from "firebase/firestore";
 import { ChangeEvent, useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
-import { firestore } from "../../firebase";
 
 interface Props {
   content: string;
@@ -9,20 +6,7 @@ interface Props {
 }
 
 const NoteTextArea: React.FC<Props> = ({ content, onChange }) => {
-  const [searchParams] = useSearchParams();
-
   const [text, setText] = useState("");
-
-  function saveContentToDB() {
-    const noteId = searchParams.get("noteId");
-
-    if (!noteId) return;
-
-    const docRef = doc(firestore, "notes", noteId);
-    updateDoc(docRef, {
-      content,
-    });
-  }
 
   function handleChange(e: ChangeEvent<HTMLTextAreaElement>) {
     setText(e.target.value);
@@ -38,16 +22,11 @@ const NoteTextArea: React.FC<Props> = ({ content, onChange }) => {
   }, [content]);
 
   return (
-    <>
-      <textarea
-        className="w-full h-full bg-transparent text-zinc-100 resize-none border-none outline-cyan-200 outline-1 p-2 text-lg whitespace-pre overflow-scroll"
-        value={text}
-        onChange={handleChange}
-      ></textarea>
-      <button className="bg-green-300" onClick={saveContentToDB}>
-        Salvar
-      </button>
-    </>
+    <textarea
+      className="w-full h-full bg-transparent text-zinc-100 resize-none border-none outline-cyan-200 outline-1 p-2 text-lg whitespace-pre overflow-scroll"
+      value={text}
+      onChange={handleChange}
+    ></textarea>
   );
 };
 
